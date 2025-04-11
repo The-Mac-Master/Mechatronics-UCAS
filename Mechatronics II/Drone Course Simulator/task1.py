fa1110416fbb4ba432200e80650ebf3d4ee831bb
The-Mac-Master <fenixstaylor@icloud.com>
2025-04-11 09:35:38 -0600

    Upadted and stuff

--- Mechatronics II/Drone Course Simulator/task1.py
+++ Mechatronics II/Drone Course Simulator/task1.py
@@ -30,5 +30,28 @@ drone.rotate_cw(90) #Rotate drone counter-clockwise 90 Degrees
 drone.forward(110) #Move drone forward 100 Meters
 drone.rotate_ccw(90) #Rotate drone counter-clockwise 90 Degrees
 drone.forward(300) #Move drone forward 100 Meters
+drone.rotate_ccw(130) #Rotate drone counter-clockwise 130 Degrees
+drone.forward(750) #Move drone forward 800 Meters
+drone.rotate_ccw(50) #Rotate drone clockwise 50 Degrees
+drone.forward(200) #Move drone forward 200 Meters
+drone.rotate_ccw(90) #Rotate drone counter-clockwise 90 Degrees
+drone.forward(150) #Move drone forward 150 Meters
+drone.up(3) #Move drone up 3 Meters
+drone.forward(100) #Move drone forward 100 Meters
+drone.down(2) #Move drone down 2 Meters
+drone.forward(100) #Move drone forward 100 Meters
+drone.down(2) #Move drone down 2 Meters
+drone.forward(225) #Move drone forward 250 Meters
+drone.rotate_ccw(90) #Rotate drone counter-clockwise 90 Degrees
+drone.forward(235) #Move drone forward 300 Meters
+drone.rotate_ccw(90) #Rotate drone counter-clockwise 90 Degrees
+drone.forward(110) #Move drone forward 100 Meters
+drone.rotate_cw(90) #Rotate drone clockwise 90 Degrees
+drone.forward(400) #Move drone forward 200 Meters
+drone.rotate_cw(90) #Rotate drone counter-clockwise 90 Degrees
+drone.forward(110) #Move drone forward 100 Meters
+drone.rotate_ccw(90) #Rotate drone counter-clockwise 90 Degrees
+drone.forward(300) #Move drone forward 100 Meters
+
 
 drone.launch() #Launch drone
